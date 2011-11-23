86bbb68380aa08a511629aeb7a5bffb05dc20e6a
Rick Waldron <waldron.rick@gmail.com>
2011-11-22 23:16:04 -0500

    Abacus.store refactor

--- src/abacus.store.js
+++ src/abacus.store.js
@@ -1,35 +1,62 @@
 (function( window, Abacus ) {
-  
+
   // Local cache for Abacus
-  var abacusCache = {};
+  var
+  localStorage = window.localStorage,
+
+  cache = {},
+
+  local = {
+    read: function() {
+      var stored = localStorage.getItem( 'Abacus' );
+
+      if ( !stored ) {
+        stored = local.write({});
+      }
+
+      return JSON.parse( localStorage.getItem( 'Abacus' ) );
+    },
+    write: function( object ) {
+      var data = Abacus.extend( cache, object );
+
+      localStorage.setItem( 'Abacus', JSON.stringify( data ) );
+
+      cache = data;
+
+      return data;
+    }
+  };
 
   // Abacus store API for storing arbitrary objects
   // Store defaults to using localStorage
   // Overwrite these methods to introduce your own persistance layer
   // TODO: introduce strategy for different Abacus entities to persist
   // at different endpoints
+
+
   Abacus.store = {
-    create: function( id, value ){
-      abacusCache[ id ] = value;
-      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );
+    create: function( id, value ) {
+      var temp = {};
+
+      temp[ id ] = value;
+
+      local.write( temp );
+
       return this;
     },
-    read: function( id ){
-      var abacusCache = JSON.parse( window.localStorage.getItem( 'Abacus') );      
-      return abacusCache[ id ];
-    },
-    update: function( id, value ){
-      Abacus.extend( abacusCache[ id ], value );
-      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );      
-      return this;
+    read: function( id ) {
+      var stored = local.read();
+
+      return id != null ? stored[ id ] : stored;
     },
     destroy: function( id ){
-      delete abacusCache[ id ];
-      window.localStorage.setItem( 'Abacus', JSON.stringify( abacusCache ) );
+      delete cache[ id ];
+
+      local.write( cache );
       return this;
-    },
-    readAll: function() {
-      return JSON.parse( window.localStorage.getItem( 'Abacus') );      
     }
   };
+
+  Abacus.store.update = Abacus.store.create;
+
 })( this, this.Abacus );
